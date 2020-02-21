e38dd8f4a303feabff34fc7f728b32aae99a60d4
Cecil <cj@lovapp.co>
2020-02-21 14:03:39 -0800

    try to work2

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ export const webscoketUrl = `ws://${domain}`;
 export const apiUrl = `http://${domain}`;
 export const mongoDBUrl =
   "mongodb://naw-admin:goviral123@ds011251.mlab.com:11251/heroku_52860pgd";
-export const redishost = "redis-15887.c12.us-east-1-4.ec2.cloud.redislabs.com";
-export const redisport = "15887";
-export const redispass = "M0p1hU32ki9JdBcvf5tX4n8wVi31dkUT";
+export const redisHost = "redis-15887.c12.us-east-1-4.ec2.cloud.redislabs.com";
+export const redisPort = "15887";
+export const redisPass = "M0p1hU32ki9JdBcvf5tX4n8wVi31dkUT";
