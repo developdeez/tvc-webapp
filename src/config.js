178360d140842626966010e159b740507c01e5c4
Cecil <cj@lovapp.co>
2020-02-21 16:34:25 -0800

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 const domain =
-  process.env.NODE_ENV === "production"
-    ? "157.245.140.249:4444"
-    : "localhost:4444";
+  process.env.NODE_ENV === "production" ? "157.245.140.249" : "localhost:4444";
 export const websocketUrl = `ws://${domain}/subscriptions`;
 export const apiUrl = `http://${domain}/graphql`;
